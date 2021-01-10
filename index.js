3537109617871e0e4aabc25490cbbbf9836c78e8
Hugo Ruiz <hruizvil@outlook.com>
2021-01-10 14:30:56 -0800

    changed monthly-payment id duplicate on line 91 of html

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log('Connected');
+
+let slider = document.getElementById("monthly-payment-slider");
+
+slider.oninput = function() {
+    let output = document.getElementById("monthly-payment-amt");
+    output.innerHTML = `$ ${slider.value}`;
+}
+
