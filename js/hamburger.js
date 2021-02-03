cfb010b1056b360265fe29c4a9137d93b478a695
Hugo Ruiz <hruizvil@outlook.com>
2021-02-03 14:25:00 -0800

    modified hamburger usability

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,29 +1,54 @@
 const hamburger = document.getElementById('hamburger');
 const navUL = document.getElementById('nav-ul');
 const navBar = document.getElementById("nav-bar");
-const openNav = document.getElementsByClassName("fa-bars");
-const closeNav = document.getElementsByClassName("fa-times");
+const closeBtn = document.getElementById("closebtn");
+const openBtn = document.getElementById("openbtn");
 const navLinks = document.querySelectorAll("#developers-modal-link, #contact-modal-link");
 const navLinksArr = [...navLinks];
 let showMenu = false;
 
-hamburger.addEventListener("click", () => {
-    let mobileScreen = window.matchMedia("(max-width: 1024px)");
-    if (mobileScreen.matches) {
-        navBar.classList.toggle('show-overlay');
-        navUL.classList.toggle('nav-ul-updated');
-        hamburger.classList.toggle('hamburger-updated');
-        if (!showMenu) {
-            openNav.classList.toggle("hide-hamburger");
-            closeNav.class.toggle("show-close-button");
-            showMenu = true;
-        } else {
-            closeNav.class.toggle("hide-hamburger");
-            openNav.classList.toggle("show-close-button");
-            showMenu = false;
-        }
-        navLinksArr.forEach(link => {
-            link.classList.toggle("nav-ul-links");
-        });
-    }
-});
+function closeMenu () {
+    document.getElementById("nav-bar").style.height = "0%";
+    navBar.classList.remove('show-overlay');
+    navUL.classList.remove('nav-ul-links');
+    openBtn.style.display = "block";
+    closeBtn.style.opacity = "0";
+}
+
+function openMenu () {
+    document.getElementById("nav-bar").style.height = "100%";
+    openBtn.style.display = "none";
+    closeBtn.style.opacity = "1";
+    navBar.classList.add('show-overlay');
+    navUL.classList.add('nav-ul-links');
+}
+
+closeBtn.addEventListener('click', () => {
+    closeMenu();
+})
+
+openBtn.addEventListener('click', () => {
+    openMenu();
+})
+
+// hamburger.addEventListener("click", () => {
+//     let mobileScreen = window.matchMedia("(max-width: 1024px)");
+//     if (mobileScreen.matches) {
+//         navBar.classList.toggle('show-overlay');
+//         navUL.classList.toggle('nav-ul-updated');
+//         hamburger.classList.toggle('hamburger-updated');
+//         if (!showMenu) {
+//             openNav.style.opacity = "0";
+//             closeNav.style.opacity = "1";
+//             showMenu = false;
+//         } else {
+//             openNav.style.opacity = "1";
+//             closeNav.style.opacity = "0";
+//             showMenu = true;
+//         }
+
+//         navLinksArr.forEach(link => {
+//             link.classList.toggle("nav-ul-links");
+//         });
+//     }
+// });
